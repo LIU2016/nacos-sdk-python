3a5fbed9149acb44baec38b8f56b063f674e3ef0
helixcs <zhangjian12424@gmail.com>
2020-08-18 22:15:13 +0800

    Add Py3 Support

--- nacos/py3/nacos_base.py
+++ nacos/py3/nacos_base.py
@@ -7,11 +7,14 @@
  Time: 2020/8/17
  Reference at:https://nacos.io/zh-cn/docs/open-api.html
 """
-
+from abc import abstractmethod, ABC
 from typing import Optional
+from nacos.exception import NacosRequestException
 from enum import Enum
 
 NACOS_CONFIG = "/nacos/v1/cs/configs"
+
+JSON_TYPE = dict
 
 
 class RequestMethods(Enum):
@@ -36,10 +39,34 @@ class NacosBaseRequest(object):
   def api_name(self) -> Optional[str]:
     return self._api_name
 
+  @property
+  def request_method(self) -> RequestMethods:
+    return self._request_method
+
+  @abstractmethod
+  def request(self) -> str:
+    raise NacosRequestException('request is abstract method,not implemented.')
+
 
 # todo
-class NacosConfiguration(NacosBaseRequest):
+class NacosConfiguration(NacosBaseRequest, ABC):
   pass
+
+
+class NacosGetConfiguration(NacosConfiguration):
+
+  def __init__(self, tenant: Optional[str], data_id: str, group: str):
+    super().__init__(NACOS_CONFIG, RequestMethods.GET)
+    # 租户信息，对应 Nacos 的命名空间ID字段
+    self._tenant = tenant
+    # 配置 ID
+    self._data_id = data_id
+    # 配置分组
+    self._group = group
+
+  # ...
+  def request(self) -> str:
+    pass
 
 
 class NacosService(NacosBaseRequest):
